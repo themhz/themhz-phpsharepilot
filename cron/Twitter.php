63736447a288740192db40f0df9d4040b414150e
themhz <themhz@gmail.com>
2023-06-01 22:59:52 +0300

    adding twitter

--- cron/Twitter.php
+++ cron/Twitter.php
@@ -1,6 +1,68 @@
 <?php
+require 'vendor/autoload.php';
+use Abraham\TwitterOAuth\TwitterOAuth;
+
 class Twitter implements ISocialMediaService{
     public function post(){
-        return "posting Twitter implementation";
+
+        //$this->uploadtweet();
+        $this->tweet();
+        //return "posting Twitter implementation";
+
+    }
+
+
+    public function uploadtweet(){
+        $consumer_key = 'cpBXk3DhyCANvdQtjpvxmLB3C';
+        $consumer_secret = 'Ar57VS71PxMY6xwiEMdg5dpV7cyb6rX74LjckK8KVYHXgF4ZdY';
+        $access_token = '78294985-68h5YCG7HNs1L9tDwdq4Vv9dkpzU6vzTht02MjSgb';
+        $access_token_secret = 'lZ1o4ik0OuPmEw9GC1wUPZJXFZ8grw5UyIMoutKN5w4WL';
+
+        $connection = new TwitterOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
+
+        // Path to the image file
+        $image_path = 'C:\Users\themhz\Pictures\melita.jpg';
+
+        // Upload image to Twitter
+        $media = $connection->upload('media/upload', ['media' => $image_path]);
+
+        // Tweet text
+        $status = 'some random text.';
+
+        // Post tweet with image
+        $post_tweets = $connection->post("statuses/update", [
+            'status' => $status,
+            'media_ids' => $media->media_id
+        ]);
+
+        print_r($post_tweets);
+
+//        if ($connection->getLastHttpCode() == 200) {
+//            echo "Tweet with image posted successfully";
+//        } else {
+//            echo "Error posting tweet";
+//        }
+    }
+    public function tweet() {
+        // You will get these keys from Twitter's developer portal after creating your application
+        $consumer_key = 'cpBXk3DhyCANvdQtjpvxmLB3C';
+        $consumer_secret = 'Ar57VS71PxMY6xwiEMdg5dpV7cyb6rX74LjckK8KVYHXgF4ZdY';
+        $access_token = '78294985-68h5YCG7HNs1L9tDwdq4Vv9dkpzU6vzTht02MjSgb';
+        $access_token_secret = 'lZ1o4ik0OuPmEw9GC1wUPZJXFZ8grw5UyIMoutKN5w4WL';
+
+        $connection = new TwitterOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
+
+        $status = 'Hello, Twitter!';
+        $connection->setApiVersion('2');
+        $post_tweets = $connection->post("tweets", ["text" => "Check out this https://fcmsoft.com/template/fcmsoft/img/screenshots/fcms1.png and more is coming "], true);
+
+
+        print_r($post_tweets);
+//        if ($connection->getLastHttpCode() == 200) {
+//            echo "Tweet posted successfully";
+//        } else {
+//            //echo "Error posting tweet";
+//        }
+
     }
 }
--- cron/worker.php
+++ cron/worker.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once '../config.php';
 require_once 'Facebook.php';
 require_once 'Instagram.php';
@@ -6,28 +7,32 @@ require_once 'TikTok.php';
 require_once 'Twitch.php';
 require_once 'Twitter.php';
 
-interface ISocialMediaService{
+interface ISocialMediaService
+{
     public function post();
 }
 
-class PostingService{
+class PostingService
+{
     private $sm = [];
 
-    public function add(ISocialMediaService $sm){
+    public function add(ISocialMediaService $sm)
+    {
         $this->sm[] = $sm;
     }
 
-    public function post(){
-        for($i=0;$i<count($this->sm);$i++){
-            echo $this->sm[$i]->post()."\n\r";
+    public function post()
+    {
+        for ($i = 0; $i < count($this->sm); $i++) {
+            echo $this->sm[$i]->post() . "\n\r";
         }
     }
 }
 
 $ps = new PostingService();
-$ps->add(new Facebook());
-$ps->add(new Instagram());
+//$ps->add(new Facebook());
+//$ps->add(new Instagram());
 $ps->add(new Twitter());
-$ps->add(new Twitch());
-$ps->add(new TikTok());
+//$ps->add(new Twitch());
+//$ps->add(new TikTok());
 $ps->post();
