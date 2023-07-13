0dcabb3f08f3167c967dfd2db394836692f9b7ba
themhz <themhz@gmail.com>
2023-07-13 19:15:24 +0300

    updating lists

---
+++ models/Lists.php
@@ -0,0 +1,1 @@
+<?phpnamespace SharePilotV2\Models;use SharePilotV2\Components\Model;class Lists extends Model{    public function GetTable()    {        return "lists";    }}
---
+++ pages/database/checkurl.php
@@ -0,0 +1,20 @@
+<!-- check url Modal -->
+<div id="modal" class="w3-modal">
+    <div class="w3-modal-content w3-animate-zoom w3-card-4">
+        <header class="w3-container w3-teal">
+            <span onclick="document.getElementById('modal').style.display='none'"
+                  class="w3-button w3-display-topright">&times;</span>
+            <h2 id="modalTitle"></h2>
+        </header>
+        <div class="w3-container">
+            <br>
+            <img id="modalImage" class="w3-image" alt="Image" style="width:50%;"/>
+            <p id="modalDescription"></p>
+            <p id="modalPostTime"></p>
+        </div>
+        <footer class="w3-container w3-teal w3-padding">
+            <button id="closeModal" class="w3-button w3-white w3-border w3-round-large" onclick="closeModal()">Close</button>
+            <input id="saveLink" class="w3-button w3-white w3-border w3-round-large" onclick="saveLink()" value="Save">
+        </footer>
+    </div>
+</div>
