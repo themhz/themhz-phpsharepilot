6702879c783b657dd16763dad7af291ecdbe816d
Θέμης Θεοτοκάτος <themis@Civiltech.com>
2024-04-03 18:19:11 +0300

    fixing insert message

--- SharePilot/index.php
+++ SharePilot/index.php
@@ -1,8 +1,10 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+
+echo "ok";
+/*require_once __DIR__ . '/vendor/autoload.php';
 use SharePilotV2\Components\Website;
 $website = new Website();
 $website->start();
-
+*/
 
 
