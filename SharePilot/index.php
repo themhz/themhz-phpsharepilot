fef81731b1f45d7924e3bce336e7b632aa1207bb
Themis Theotokatos <themhz@gmail.com>
2024-06-05 11:36:07 +0300

    adding update mechanism

--- SharePilot/index.php
+++ SharePilot/index.php
@@ -1,6 +1,8 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+
+ require_once __DIR__ . '/vendor/autoload.php';
  use SharePilotV2\Components\Website;
+
  $website = new Website();
  $website->start();
 
