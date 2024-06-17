9a0dc7e34bab3542869ffc20d1d1690245476c50
Themis Theotokatos <themhz@gmail.com>
2024-06-17 14:35:37 +0300

    testing new release update mechanism

--- SharePilot/pages/testpage/testpage.php
+++ SharePilot/pages/testpage/testpage.php
@@ -1,1 +1,14 @@
-Hello test page
+<div class="w3-row custom-padding-45">
+        <div class="w3-container w3-teal">
+            <h1>Hello test page</h1>
+        </div>
+        <div class="w3-container">
+            <div class="w3-panel w3-leftbar">
+                <p><i class="fa fa-quote-right w3-xxlarge"></i><br>
+                    <i class="w3-serif w3-xlarge">Hello test page
+
+                    </i>
+                </p>
+            </div>
+        </div>
+    </div>
